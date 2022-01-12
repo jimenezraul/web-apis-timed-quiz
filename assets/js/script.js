7851a903c31246508c725a543d1732692f3ec774
Raul <jimenezraul1981@gmail.com>
2022-01-12 12:23:25 -0500

    added high score page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,58 @@
 // Set all the variable to the elements
 var startBox = document.querySelector(".start-quiz");
 var quizBox = document.querySelector(".quiz-box");
+var initialsBox = document.querySelector(".initials-box");
 var start_btn = document.getElementById("start");
 var questionTitle = document.querySelector(".question");
 var answer = document.querySelector(".answers");
+var finalScoreEl = document.querySelector("#final-score");
 var questionCount = 0;
 var resultShow = document.querySelector(".result");
+var timerEl = document.querySelector("#timer");
+var startTime = 70;
+var penalize = 0;
+var score = 0;
+
+//End quiz
+var endQuiz = function () {
+  quizBox.classList.add("hide");
+  initialsBox.classList.remove("hide");
+  finalScoreEl.textContent = score;
+};
+
+// Quiz timer
+var timer = function () {
+  var timerstart = setInterval(function () {
+    startTime--;
+    if (penalize > 0) {
+      startTime = startTime - penalize;
+    }
+    if (initialsBox.classList.contains("hide")) {
+      timerEl.textContent = startTime;
+    } else {
+      timerEl.textContent = score
+    }
+    penalize = 0;
+    if (startTime <= 0) {
+      clearInterval(timerstart);
+      endQuiz();
+    }
+  }, 1000);
+};
 
 // Selected answer
-var selectedAnswer = function(e, answer) {
-  console.log(e.target.getAttribute("value") === answer)
-}
+var selectedAnswer = function (e, answer) {
+  if (e.target.getAttribute("value") === answer) {
+    resultShow.classList.remove("hide");
+    resultShow.innerHTML = "<p>Correct!</p>";
+    nextQuestions();
+  } else {
+    resultShow.classList.remove("hide");
+    resultShow.innerHTML = "<p>Wrong!</p>";
+    penalize = 10;
+    nextQuestions();
+  }
+};
 
 // Show question to the page
 var showQuestion = function (question) {
@@ -20,17 +62,24 @@ var showQuestion = function (question) {
     var liEl = document.createElement("li");
     liEl.textContent = i + 1 + ". " + question.options[i];
     liEl.setAttribute("class", "answer");
-    liEl.setAttribute("value", question.options[i])
-    liEl.addEventListener("click", function(e){ selectedAnswer(e, question.answer)})
+    liEl.setAttribute("value", question.options[i]);
+    liEl.addEventListener("click", function (e) {
+      selectedAnswer(e, question.answer);
+    });
     olEl.appendChild(liEl);
+    answer.appendChild(olEl);
   }
-  answer.appendChild(olEl);
 };
 
 // Reset question card and set the next question
 var nextQuestions = function () {
   resetEl();
-  showQuestion(questions[questionCount]);
+  if (questions[questionCount] === undefined) {
+    score = startTime;
+    endQuiz();
+  } else {
+    showQuestion(questions[questionCount]);
+  }
   questionCount++;
 };
 
@@ -38,8 +87,7 @@ var nextQuestions = function () {
 var start_quiz = function () {
   startBox.classList.add("hide");
   quizBox.classList.remove("hide");
-  shuffledQuestion = Math.random() - 0.5;
-  console.log(shuffledQuestion);
+  timer(startTime);
   nextQuestions();
 };
 
@@ -53,9 +101,29 @@ var resetEl = function () {
 // Questions array
 const questions = [
   {
-    question: "This is an example",
-    answer: "Example 3",
-    options: ["Example 1", "Example 2", "Example 3", "Example 4"],
+    question: "Commonly used data types DO Not Include:",
+    answer: "alerts",
+    options: ["strings", "booleans", "alerts", "numbers"],
+  },
+  {
+    question: "The consition in a if / else statement is enclosed with _______.",
+    answer: "parenthesis",
+    options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+  },
+  {
+    question: "Arrays in Javascript can be used to store _______.",
+    answer: "all of the above",
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+  },
+  {
+    question: "String values must be enclosed within _______ when being assined to variables.",
+    answer: "quotes",
+    options: ["commas", "curly brackets", "quotes", "parenthesis"],
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "console.log",
+    options: ["Javascript", "terminal/bash", "for loops", "console.log"],
   },
 ];
 
